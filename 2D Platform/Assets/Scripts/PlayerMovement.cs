0ac20f860f1bcdef9929ec92c1cb542bcd56ef24
TranHuuMinhDuc <ytientvhue@gmail.com>
2024-11-09 12:21:38 +0700

    Update( Animation, CheckGround)

--- 2D Platform/Assets/Scripts/PlayerMovement.cs
+++ 2D Platform/Assets/Scripts/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,17 +8,27 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f;
     public float jumpSpeed = 8f;
     private float direction = 0f;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    private bool isTouchingGround;
+
+   
     private Rigidbody2D player;
+    private Animator playerAnimation;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponent<Rigidbody2D>();
+        playerAnimation = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        isTouchingGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
         direction = Input.GetAxis("Horizontal");
         if(direction > 0f)
         {
@@ -32,9 +43,14 @@ public class PlayerMovement : MonoBehaviour
             player.velocity = new Vector2(0, player.velocity.y);
         }
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isTouchingGround == true)
         {
             player.velocity = new Vector2(player.velocity.x, jumpSpeed);
         }
+
+        playerAnimation.SetFloat("Speed", Mathf.Abs(player.velocity.x));
+        playerAnimation.SetBool("OnGround", isTouchingGround);
+
     }
+
 }
